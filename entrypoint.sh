1a1df5538e254dbe5514286e0cae714eaf9434c7
Dave Horton <daveh@beachdognet.com>
2022-01-28 10:11:20 -0500

    update to rtpengine mr10.2.1.5 and add support for IMSv2 on aws

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,13 @@ case $CLOUD in
     PUBLIC_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
     ;;
   aws)
-    LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-    PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+    if [ -z "$IMDSv2" ]; then
+      LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+      PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+    else 
+      LOCAL_IP=$(TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
+      PUBLIC_IP=$(TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+    fi
     ;;
   digitalocean)
     LOCAL_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
@@ -36,7 +41,7 @@ if [ -z "$RTP_END_PORT" ]; then
   RTP_END_PORT=60000
 fi
 if [ -z "$LOGLEVEL" ]; then
-  LOGLEVEL=6
+  LOGLEVEL=5
 fi
 
 if [ "$1" = 'rtpengine' ]; then
